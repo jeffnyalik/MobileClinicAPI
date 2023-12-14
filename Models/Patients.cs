9cedab1ed28e36e9fe8392c7db22cd6d495a7ba9
jeffnyak@gmail.com <jeffnyak@gmail.com>
2023-12-14 10:13:52 +0300

    Add project files.

---
+++ Models/Patients.cs
@@ -0,0 +1,34 @@
+﻿namespace MobileClinicAPI.Models
+{
+    public class Patients
+    {
+        // Patient.cs
+        public class Patient
+        {
+            public int PatientID { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string OtherName { get; set; }
+            public DateTime? DateOfBirth { get; set; }
+            public string Gender { get; set; }
+            public string IDNumber { get; set; }
+            public string HomeCounty { get; set; }
+            public string Mobile { get; set; }
+            public string Email { get; set; }
+            public string NameOfAlternative { get; set; }
+            public string TelephoneNumber { get; set; }
+            public bool HasDisability { get; set; }
+        }
+
+        // Appointment.cs
+        public class Appointment
+        {
+            public int AppointmentID { get; set; }
+            public int PatientID { get; set; }
+            public int DoctorID { get; set; }
+            public DateTime? AppointmentDate { get; set; }
+            public TimeSpan? AppointmentTime { get; set; }
+        }
+
+    }
+}
